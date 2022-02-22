e23b282c8f4c4d79e449574b315c1e74f48aa34c
Simone_Berti <foto.berti55@gmail.com>
2022-02-22 12:54:42 +0100

    Aggiunta di un riferimento a un all'atro progetto per usare le sue classi

--- src/it/corso/java/CorsoJava.java
+++ src/it/corso/java/CorsoJava.java
@@ -1,14 +1,16 @@
 package it.corso.java;
 
+import Corso.lezione.Calcolatore.Calcolatrice;
+
 import java.io.File;
 
 public class CorsoJava {
+    /*Variabili di classe*/
+    public static String j = "classe";
+
     /*Variabili d'istanza*/
     private final int contatore = 0;
     int s = 5;
-
-    /*Variabili di classe*/
-    public static String j = "classe";
 
     /*Costruttori*/
     public CorsoJava() {
@@ -19,16 +21,28 @@ public class CorsoJava {
         System.out.println("hellos");
         /*Variabile locale*/
         int a = 10;
-        CorsoJava c = new CorsoJava();
+        CorsoJava c = new CorsoJava(); /*Due in uno Dichiarazione + Inizializzazione Molto Easy se si sa già ma è giusto per ripassare un po di nozioni*/
         int s = c.s;
+
+        System.out.println(CorsoJava.j);
+
+        CorsoJava c1; /*Dichiarazione*/
+        c1 = new CorsoJava();/*Inizializzazione*/
+
+        int p = 10;
+        int b = 20;
+
+        Calcolatrice cal = new Calcolatrice();
+        cal.somma(p,b);
     }
 
     /*Metodi*/
     private String test() {
         return "ok";
     }
+
     /*Parametri ARGS*/
-    public String concatena(String string1, String string2, double c, File file){
+    public String concatena(String string1, String string2, double c, File file) {
         return string1.concat(string2);
     }
 }
